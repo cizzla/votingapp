f991e940fa932d3e836dfdf56b9353f00374eba7
cizzla <fredwaweru47@gmail.com>
2026-06-08 10:09:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { Pool } = require('pg');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
